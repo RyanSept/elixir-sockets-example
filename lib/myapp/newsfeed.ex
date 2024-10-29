a118e63949578640329ae2055abd73857de28fd3
Ryan Marvin <marvinryan@ymail.com>
2024-10-29 13:03:07 +0000

    Create newsfeed lib

---
+++ lib/myapp/newsfeed.ex
@@ -0,0 +1,10 @@
+defmodule Myapp.Newsfeed do
+  defstruct author: nil, body: nil, time: nil
+
+  def get_feed(),
+    do: [
+      %__MODULE__{author: "user1", body: "It do be like that", time: ~U[2025-01-01 12:00:00Z]},
+      %__MODULE__{author: "user2", body: "Covfefe again", time: ~U[2025-01-01 12:01:00Z]},
+      %__MODULE__{author: "user3", body: "It's giving", time: ~U[2025-01-01 12:02:00Z]}
+    ]
+end
